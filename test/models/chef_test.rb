2542143b936e1a0318d671e7729ab01532843805
Tom Lewis <tlewismedia@gmail.com>
2016-01-15 05:03:27 +0000

    add chef and recipe test, tests are prducing errors

---
+++ test/models/chef_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class ChefTest < ActiveSupport::TestCase
+  
+  def setup
+    @chef = Chef.new(chefname: "john", email: "john@example.com")
+  end
+  
+  test "chef should be valid" do
+    assert @chef.valid?
+  end
+  
+  test "chefname should be present" do
+    @chef.chefname = " "
+    assert_not @chef.valid?
+  end
+  
+end
---
+++ test/models/recipe_test.rb
@@ -0,0 +1,46 @@
+require 'test_helper'
+
+class RecipeTest < ActiveSupport::TestCase
+  
+  def setup
+    @recipe = Recipe.new(name: "chicken parm", summary: "this is the best chicken parm recipe ever",
+              description: "here is the description for this recipe")
+  end
+  
+  test "recipe should be valid" do
+    assert @recipe.valid?
+  end
+  
+  test "name should be present" do
+    @recipe.name = " "
+    assert_not @recipe.valid?
+  end
+  
+  test "name length should not be too long" do
+    @recipe.name = "a"*101
+    assert_not @recipe.valid?
+  end
+  
+  test "name length should not be too short" do
+    @recipe.name = "aaaa"
+    assert_not @recipe.valid?
+  end
+  
+  test "summary should be present" do
+    @recipe.summary = " "
+    assert_not @recipe.valid?
+  end
+  
+  test "summary length should not be too long" do
+    @recipe.summary = "a"*150
+    assert_not @recipe.valid?
+  end
+  
+  test "summary length should not be too short" do
+    @recipe.summary = "a"*9
+    assert_not @recipe.valid?
+  end
+  
+ 
+  
+end
