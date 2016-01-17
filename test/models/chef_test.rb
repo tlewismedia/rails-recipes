f675c01f987bd1b8502d8328cc741c6f24c9a879
Tom Lewis <tlewismedia@gmail.com>
2016-01-17 20:20:19 +0000

    fix database issue, and random ruby environmet change, fix recipt model synax

--- test/models/chef_test.rb
+++ test/models/chef_test.rb
@@ -15,4 +15,23 @@ class ChefTest < ActiveSupport::TestCase
     assert_not @chef.valid?
   end
   
+  test "chefname should not be too long" do
+    @chef.chefname = "a" * 41
+    assert_not @chef.valid?
+  end
+  
+  test "chefname should not be too short" do 
+    @chef.chefname = "aa"
+    assert_not @chef.valid?
+  end
+  
+  test "email should be present" do
+    assert_not @chef.valid?
+  end
+  
+  test "email length should be within bounds" do
+    @chef.email = "a" * 101 + "@example.com"
+    assert_not @chef.valid?
+  end
+  
 end
